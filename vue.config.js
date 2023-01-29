c58192e0749cffc1ac8cc35984c2e588b5570580
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-01-29 11:02:10 -0500

    remove CDN
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- vue.config.js
+++ vue.config.js
@@ -13,23 +13,6 @@ module.exports = {
         "stream": require.resolve("stream-browserify"),
       },
     },
-    plugins: [
-      ...(
-        process.env.VUE_APP_CDN
-          ? [
-            new WebpackCdnPlugin({
-              modules: [
-                { name: 'vue', var: 'Vue', path: 'dist/vue.runtime.min.js' },
-                { name: 'vue-router', var: 'VueRouter', path: 'dist/vue-router.min.js' },
-                { name: '@fortawesome/fontawesome-free', style: 'css/all.min.css', cssOnly: true },
-                { name: 'vue3-storage', var: 'Vue3StoragePlugin', path: 'dist/vue3-storage.min.js' },
-                { name: 'axios', path: 'dist/axios.min.js' },
-                { name: 'vue-i18n', var: 'VueI18n', path: 'dist/vue-i18n.min.js' },
-              ],
-              prodUrl: npm_url(':name', ':version', ':path'),
-            })]
-          : [])
-    ]
   },
   chainWebpack: config => {
     config
