3e06575d4717aa2a0c9a9d04315b028e92acf61f
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-02-01 20:37:24 -0500

    Update registerServiceWorker.js
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -3,7 +3,7 @@
 import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
-  register(process.env.BASE_URL ? process.env.BASE_URL : '/dpgui/' + `service-worker.js`, {
+  register((process.env.BASE_URL ? process.env.BASE_URL : '/dpgui/') + `service-worker.js`, {
     ready () {
       console.log(
         'App is being served from cache by a service worker.\n' +
