cb4ca27dac2c7a4dcdd191a7d200627b8722a168
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-10-06 16:15:56 -0400

    export start_dpgui
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- dpgui/__init__.py
+++ dpgui/__init__.py
@@ -1,9 +1,13 @@
 """DP-GUI Python package."""
 import logging
 
+from .web import run as start_dpgui
+
 logger = logging.getLogger(__name__)
 handler = logging.StreamHandler()
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
+
+__all__ = ["start_dpgui"]
