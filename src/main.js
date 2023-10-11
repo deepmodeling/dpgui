bf9184070051ede92881d84d7d276d1769a80211
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-10-11 16:49:33 -0400

    enable treeshaking for fontawesome icons (#282)
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
    
    ---------
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- src/main.js
+++ src/main.js
@@ -7,11 +7,19 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import './registerServiceWorker'
 import i18n from './i18n'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// only import used icons
+import { faLanguage, faHome, faKeyboard, faCog, faPlusCircle, faTrash, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 
 const app = createApp({
   render: () => h(App),
 })
+
+app.component('FontAwesomeIcon', FontAwesomeIcon)
+library.add(faLanguage, faHome, faKeyboard, faCog, faPlusCircle, faTrash, faPlus)
+
 app.use(vuetify)
 app.use(router)
 app.use(i18n)
